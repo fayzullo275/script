00780247dd503c408a79cae3005254e6d9e12d37
Fayzullo <@Fayzullo>
2022-11-16 16:10:07 +0500

    script

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+var name = prompt ("Ismingizni kiriting")
+console.log ("Sizning ismingiz " + name);
+var age = +prompt ("Yoshingizni Kiriting")
+console.log("Sizning Yoshingiz " +age);
+var misol1 = +prompt("Misolni yeching " + "15+15=")
+console.log("Misol-1 "+ '15+15=' + (15+15) + " Sizning Javobingiz " + misol1); 
+var misol2 = +prompt("Misolni yeching " + "20-8=")
+console.log("Misol-2 "+ '20-8=' + (20-8) + " Sizning Javobingiz " + misol2);
+var misol3 = +prompt("Misolni yeching " + "25*7=")
+console.log("Misol-3 "+ '25*7=' + (25*7) + " Sizning Javobingiz " + misol3);
+var misol4 = +prompt("Misolni yeching " + "30/2=")
+console.log("Misol-4 "+ '30/2=' + (30/2) + " Sizning Javobingiz " + misol4);
+var misol5 = +prompt("Misolni yeching " + "10%3=")
+console.log("Misol-5 "+ '10%3=' + (10%3) + " Sizning Javobingiz " + misol5);
+var consol =prompt("Javoblaringizni console oynasida ko'rishingiz mumkun!")
